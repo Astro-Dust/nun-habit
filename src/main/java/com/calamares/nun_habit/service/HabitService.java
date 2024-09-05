5cc66815ba6762389d53640f7810b6c1c65aa52b
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-05 10:13:00 -0300

    addHabit and markAsCompleted methods added

---
+++ src/main/java/com/calamares/nun_habit/service/HabitService.java
@@ -0,0 +1,27 @@
+package com.calamares.nun_habit.service;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.calamares.nun_habit.model.Habit;
+
+// nessa classe ficam os métodos principais
+public class HabitService {
+    
+    List<Habit> habits = new ArrayList<>();
+
+    public List<Habit> addHabit(Habit habit) {
+        habits.add(habit);
+        return habits;
+    }
+
+    public void markAsCompleted(Long id) {
+        for (Habit habit : habits) {
+            if (habit.getId().equals(id)) {
+                habit.setCompleted(true);
+                break;
+            }
+        }
+
+    }
+
+}
