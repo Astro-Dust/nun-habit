5fbe8dd455bfd5f7ec495e6376aac43dceb82b8b
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-05 09:57:47 -0300

    Habit model added

---
+++ src/main/java/com/calamares/nun_habit/model/Habit.java
@@ -0,0 +1,49 @@
+package com.calamares.nun_habit.model;
+
+public class Habit {
+    
+    private Long id;
+    private String name;
+    private String description;
+    private boolean completed;
+
+    public Habit(
+        Long id,
+        String name,
+        String description
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+}
