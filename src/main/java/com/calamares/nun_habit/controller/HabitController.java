ddca65ce5a470cd4dfa0a808dc7debc73afcb0b8
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-05 11:29:38 -0300

    getmapping added

--- src/main/java/com/calamares/nun_habit/controller/HabitController.java
+++ src/main/java/com/calamares/nun_habit/controller/HabitController.java
@@ -2,6 +2,7 @@ package com.calamares.nun_habit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.calamares.nun_habit.model.Habit;
 import com.calamares.nun_habit.service.HabitService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -30,6 +33,11 @@ public class HabitController {
     @PutMapping("/habits/{id}/complete")
     public void completeHabit(@PathVariable Long id) {
         habitService.markAsCompleted(id);
+    }
+
+    @GetMapping("/habits")
+    public List<Habit> getAllHabits() {
+        return habitService.listAllHabits();
     }
 
 }
