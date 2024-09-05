e7e198742ce34bec51d3caf0452069ca65f91655
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-09-05 09:45:12 -0300

    first commit

---
+++ src/main/java/com/calamares/nun_habit/NunHabitApplication.java
@@ -0,0 +1,13 @@
+package com.calamares.nun_habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunHabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calamares/nun_habit/NunHabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calamares.nun_habit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunHabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
